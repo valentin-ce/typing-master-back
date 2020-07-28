bb4505c6bfb3750602cd7d76724e255aafbfe153
valentin <valentin.cellier@gmail.com>
2020-07-28 14:50:45 +0200

    update

--- api-typing-master/index.js
+++ api-typing-master/index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 4000;
+const cors = require("cors");
 
 /* import queries */
 const users = require('./queries/users');
 
+
+app.use(cors());
 app.use(bodyParser.json())
 app.use(
     bodyParser.urlencoded({
